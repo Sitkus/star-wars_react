0438f862d1d60fb8037235bb26b91d8fc7fafa89
Sitkus <65811246+Sitkus@users.noreply.github.com>
2020-11-21 20:28:41 +0200

    Added fetching for data, but there are some bugs still that need to be fixed, mainly with TypeScript types.

--- src/components/pages/FirstTab.tsx
+++ src/components/pages/FirstTab.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { makeStyles, Theme } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
@@ -13,6 +13,7 @@ import Container from '@material-ui/core/Container';
 
 import SearchIcon from '@material-ui/icons/Search';
 import InputBase from '@material-ui/core/InputBase';
+import { NamedTupleMember } from 'typescript';
 
 const useStyles = makeStyles((theme: Theme) => ({
   gridContainer: {
@@ -22,10 +23,7 @@ const useStyles = makeStyles((theme: Theme) => ({
     minWidth: 650
   },
   container: {
-    marginBottom: '70px'
-  },
-  tableContainer: {
-    padding: '50px'
+    marginBottom: '30px'
   },
   tableRounded: {
     borderRadius: '15px',
@@ -48,6 +46,7 @@ const useStyles = makeStyles((theme: Theme) => ({
     //   backgroundColor: fade(theme.palette.common.white, 0.25),
     // },
     marginLeft: 'auto',
+    marginBottom: '40px',
     width: '200px',
     borderBottom: '1px solid black',
     [theme.breakpoints.up('sm')]: {
@@ -66,6 +65,7 @@ const useStyles = makeStyles((theme: Theme) => ({
   },
   inputRoot: {
     color: 'inherit',
+    fontSize: '14px'
   },
   inputInput: {
     // padding: theme.spacing(1, 1, 1, 0),
@@ -77,31 +77,62 @@ const useStyles = makeStyles((theme: Theme) => ({
       width: '20ch',
     },
   },
+  tableCell: {
+    padding: '7px 50px 7px 0'
+  }
 }));
 
-function createData(name: string, calories: number, fat: number, carbs: number, protein: number) {
-  return { name, calories, fat, carbs, protein };
-}
+// function createData(name: string, calories: number, fat: number, carbs: number, protein: number) {
+//   return { name, calories, fat, carbs, protein };
+// }
 
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
+// const rows = [
+
+//   // createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+//   // createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+//   // createData('Eclair', 262, 16.0, 24, 6.0),
+//   // createData('Cupcake', 305, 3.7, 67, 4.3),
+//   // createData('Gingerbread', 356, 16.0, 49, 3.9),
+// ];
 
 const FirstTab = () => {
   const classes = useStyles();
+  const [data, setData] = useState([]);
+  let [homes, setHomes] = useState([]);
+  const [homeWorlds, setHomeWorlds] = useState([]);
+
+  const getStarWarsData = async () => {
+    const response = await fetch('https://swapi.dev/api/people/');
+    const result = await response.json();
+
+    result.results.forEach((character: any) => {
+      homes = [...homes, character.homeworld] as any;
+    });
+
+    setData(result.results);
+
+    homes.forEach((url: any, index: any) => {
+      getHomeWorlds(url, index);
+    });
+  }
+
+  const getHomeWorlds = async (url: string, index: number) => {
+    const response = await fetch(url);
+    const result = await response.json() as any;
+
+    // Fix this bug
+    homeWorlds.splice(index, 0, result.name);
+    setHomeWorlds([...homeWorlds]);
+  }
+  
+  useEffect(() => {
+    getStarWarsData();
+  }, []);
 
   return (
     <Grid item>
       {/* Work on making Grid item a container as well and styling it. */}
-      <TableContainer 
-        component={Paper}
-        variant="outlined"
-        className={classes.tableRounded}
-      >
+      <TableContainer component={Paper} variant="outlined" className={classes.tableRounded}>
         <Container className={classes.container}>
           <Typography className={classes.h1} variant="h1">Star wars</Typography>
           <Typography className={classes.h2} variant="h2">Star wars heroes from swapi api</Typography>
@@ -111,7 +142,7 @@ const FirstTab = () => {
             <SearchIcon />
           </div>
           <InputBase
-            placeholder="search company"
+            placeholder="search company..."
             classes={{
               root: classes.inputRoot,
               input: classes.inputInput,
@@ -122,23 +153,25 @@ const FirstTab = () => {
         <Table className={classes.table}>
           <TableHead>
             <TableRow>
-              <TableCell padding="none">Name</TableCell>
-              <TableCell>Birth date</TableCell>
-              <TableCell>Gender</TableCell>
-              <TableCell>Home world</TableCell>
+              <TableCell className={classes.tableCell} padding="none">Name</TableCell>
+              <TableCell className={classes.tableCell} padding="none">Birth date</TableCell>
+              <TableCell className={classes.tableCell} padding="none">Gender</TableCell>
+              <TableCell className={classes.tableCell} padding="none">Home world</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
-            {rows.map((row) => (
-              <TableRow key={row.name}>
-                <TableCell padding="none" component="th" scope="row">
-                  {row.name}
-                </TableCell>
-                <TableCell>{row.calories}</TableCell>
-                <TableCell>{row.fat}</TableCell>
-                <TableCell>{row.carbs}</TableCell>
-              </TableRow>
-            ))}
+            {
+              data.map((character: any, index: NamedTupleMember) => (
+                <TableRow key={character.name}>
+                  <TableCell className={classes.tableCell} padding="none" component="th" scope="row">
+                    {character.name}
+                  </TableCell>
+                  <TableCell className={classes.tableCell} padding="none">{character.birth_year}</TableCell>
+                  <TableCell className={classes.tableCell} padding="none">{character.gender}</TableCell>
+                  <TableCell className={classes.tableCell} padding="none">{homeWorlds[index]}</TableCell>
+                </TableRow>
+              ))
+            }
           </TableBody>
         </Table>
       </TableContainer>
